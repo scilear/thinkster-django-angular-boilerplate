ffb44ae4a1c002ea81e189ff19e85537193a9679
scilear <scilear@gmail.com>
2015-08-21 13:16:17 +0200

    trying to fix patterns issue

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -3,10 +3,12 @@ from rest_framework_nested import routers
 
 from authentication.views import AccountViewSet
 
+import django
+
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
 
-urlpatterns = patterns(
+urlpatterns = django.conf.urls.patterns(
      '',
     # ... URLs
     url(r'^api/v1/', include(router.urls)),
